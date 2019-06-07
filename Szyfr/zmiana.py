0b5475e3df8ae9dc71eeb99f7dffd40f01ec2fd0
Adrian <adi.moto.woj@gmail.com>
2019-06-07 12:17:50 +0200

    v3

--- Szyfr/zmiana.py
+++ Szyfr/zmiana.py
@@ -1,8 +1,20 @@
-def znajdz (text):
-    s= list(text)
-    tab=[['a','b','c'],['d','e','f'],['g','h','i']]
-    p=text.find(tab[0][0])
-    s[p]= tab[2][0]
-    print(s)
+class zmiana:
+    def znajdz (text):
+        s= list(text)
+        tab=[['a','b','c'],['d','e','f'],['g','h','i']]
+        for i in range(2):
+            p=text.find(tab[i-1][0])
+            s[p]= tab[i+1][0]
+            p=text.find(tab[i-1][1])
+            s[p]= tab[i+1][1]
+            p=text.find(tab[i-1][2])
+            s[p]= tab[i+1][2]
+            p=text.find(tab[i][0])
+            s[p]= tab[i+1][0]
+            p=text.find(tab[i][1])
+            s[p]= tab[i+1][1]
+            p=text.find(tab[i][2])
+            s[p]= tab[i+1][2]
+        print(s)
 
-znajdz('asdr')
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dppPackage',
-    version='0.2',
+    version='0.3',
     packages=['Szyfr'],
     license='MIT',
     description='Moja biblioteka',
