9da1bf4564ed5d1a13d5291796319813026d445c
Adrian <adi.moto.woj@gmail.com>
2019-06-07 13:14:57 +0200

    v1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dppPackage',
-    version='0.3',
+    version='0.1',
     packages=['Szyfr'],
     license='MIT',
     description='Moja biblioteka',
