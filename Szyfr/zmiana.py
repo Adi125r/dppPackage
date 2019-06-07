4825af36452896ff70dc4a4fff13ae53d374ceb3
Adrian <adi.moto.woj@gmail.com>
2019-06-07 12:36:38 +0200

    v3.1

--- Szyfr/zmiana.py
+++ Szyfr/zmiana.py
@@ -1,20 +1,20 @@
-class zmiana:
-    def znajdz (text):
-        s= list(text)
-        tab=[['a','b','c'],['d','e','f'],['g','h','i']]
-        for i in range(2):
-            p=text.find(tab[i-1][0])
-            s[p]= tab[i+1][0]
-            p=text.find(tab[i-1][1])
-            s[p]= tab[i+1][1]
-            p=text.find(tab[i-1][2])
-            s[p]= tab[i+1][2]
-            p=text.find(tab[i][0])
-            s[p]= tab[i+1][0]
-            p=text.find(tab[i][1])
-            s[p]= tab[i+1][1]
-            p=text.find(tab[i][2])
-            s[p]= tab[i+1][2]
-        print(s)
+def znajdz (text):
+    s= list(text)
+    tab=[['a','b','c'],['d','e','f'],['g','h','i']]
+    for i in range(2):
+        p=text.find(tab[i-1][0])
+        s[p]= tab[i+1][0]
+        p=text.find(tab[i-1][1])
+        s[p]= tab[i+1][1]
+        p=text.find(tab[i-1][2])
+        s[p]= tab[i+1][2]
+        p=text.find(tab[i][0])
+        s[p]= tab[i+1][0]
+        p=text.find(tab[i][1])
+        s[p]= tab[i+1][1]
+        p=text.find(tab[i][2])
+        s[p]= tab[i+1][2]
+    print(s)
+
 
 
